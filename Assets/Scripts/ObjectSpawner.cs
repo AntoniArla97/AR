1b86548d07556ad1cc4bbbaf70a35c8b21535e13
AntoniArlandis <daviuarlandis@gmail.com>
2020-04-06 15:05:41 +0200

    First commit

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private PlacementIndicatorScript placementIndicatorScript;
+
+    private void Start()
+    {
+        placementIndicatorScript = FindObjectOfType<PlacementIndicatorScript>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(objectToSpawn, placementIndicatorScript.transform.position, placementIndicatorScript.transform.rotation);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PortalPlacementScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalPlacementScript : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        RaycastHit hit;
+        
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+}
